572052cf5a41728016f1e311694b7197b0d089c4
huangshan108 <shanhuang108@gmail.com>
2015-02-12 18:54:53 -0800

    assets and skeleton added

---
+++ app/controllers/account_controller.rb
@@ -0,0 +1,13 @@
+class AccountController < ApplicationController
+  def index
+  end
+
+  def login
+  end
+
+  def signup
+  end
+
+  def logout
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
+  root 'account#index'
+
+  get 'account/index'
+
+  get 'account/login'
+
+  get 'account/signup'
+
+  get 'account/logout'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
