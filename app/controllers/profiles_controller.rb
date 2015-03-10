840d6a932880fb40e20d8237e8618690a23c7710
Randy Wei <rweics@gmail.com>
2015-03-10 00:45:05 -0700

    Profile update(saving) done

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -11,6 +11,22 @@ class ProfilesController < ApplicationController
     @all_majors = Major.all
   end
 
+  def update_student
+    student = Student.find(params[:id])
+    student.update_attributes(:first_name => params[:first_name],
+                              :last_name => params[:last_name],
+                              :college_id => params[:college_id],
+                              :major_id => params[:major_id],
+                              :resume_link => params[:resume_link])
+    if student.save
+      flash[:notice] = "Profile Updated!"
+      redirect_to(single_student_profile_path(student))
+    else
+      flash[:error] = "Profile failed to update!"
+      redirect_to(edit_student_profile_path(student))
+    end
+  end
+
   def list_students
   	@all_students = Student.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   get 'profiles/students/' => 'profiles#list_students'
   get 'profiles/student/:id' => 'profiles#student', :as => :single_student_profile
   get 'profiles/student/:id/edit' => 'profiles#edit_student', :as => :edit_student_profile
+  post 'profiles/student/:id/update' => 'profiles#update_student', :as => :update_student_profile
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
