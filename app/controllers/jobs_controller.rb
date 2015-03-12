9b84dd4c8c2379fe720bf7123103c13fa3b06e84
Mushuwushu <walex692@gmail.com>
2015-03-12 15:10:46 -0700

    Fixed conflicts, job and job_list work together.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,5 @@
 class JobsController < ApplicationController
 
-<<<<<<< HEAD
 	def index
 		pages = 10
 		if params[:sort]
@@ -15,31 +14,22 @@ class JobsController < ApplicationController
 		end
 	end
 
+	def show
+	id = params[:id] # retrieve job ID from URI route
+	@job = Job.find_by_id(id) # look up job by unique ID
+	end
+
+	def create
+	  @job = Job.create!(params[:job])
+	end
+
+	def apply
+	id = params[:id]
+	@job = Job.find_by_id(id)
+	end
+
+	def user_params
+	params.require(:jobs).permit(:title, :company, :description, :salary)
+	end
 
 end
-=======
-  def show
-    id = params[:id] # retrieve job ID from URI route
-    @job = Job.find_by_id(id) # look up job by unique ID
-  end
-
-
-  def index
-    
-  end
-
-  def create
-      @job = Job.create!(params[:job])
-  end
-
-  def apply
-    id = params[:id]
-    @job = Job.find_by_id(id)
-  end
-
-  def user_params
-    params.require(:jobs).permit(:title, :company, :description, :salary)
-  end
-
-end
->>>>>>> origin/iteration1-2-viewjob
