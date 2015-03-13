dfcabba7f69ddbdddfe176a1359ef2a9872de0fa
Mushuwushu <walex692@gmail.com>
2015-03-12 20:17:43 -0700

    test

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -12,14 +12,14 @@ class JobsController < ApplicationController
 		end
 	end
 
-	def show
-	id = params[:id] # retrieve job ID from URI route
-	@job = Job.find_by_id(id) # look up job by unique ID
-	end
+	# def show
+	# id = params[:id] # retrieve job ID from URI route
+	# @job = Job.find_by_id(id) # look up job by unique ID
+	# end
 
-	def create
-	  @job = Job.create!(params[:job])
-	end
+	# def create
+	#   @job = Job.create!(params[:job])
+	# end
 
 	def apply
 	id = params[:id]
