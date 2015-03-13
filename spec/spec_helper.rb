351c7a4c3dadaf42e1f342d529d0f57730a31f3d
Pedalio <pedalioaspare@gmail.com>
2015-03-13 09:10:42 -0700

    main page complete merge

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 require 'simplecov'
 SimpleCov.start 'rails'
-=======
->>>>>>> iteration1-2-main-page
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
