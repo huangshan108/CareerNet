4142cc46c4d832ab43f3b7351180e8ea836be599
saasbook <saasbook@saasbook.(none)>
2015-03-13 18:27:15 -0300

    rspec test pass with fake user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,16 @@ class ApplicationController < ActionController::Base
 			return true
 		end
 	end
+
+    # Private method used to login as a fake user
+    # Credit: http://library.edgecase.com/oath-and-capybara
+    private
+
+    if Rails.env.test?
+      prepend_before_filter :stub_current_user
+
+      def stub_current_user
+        session[:user_id] = cookies[:stub_user_id] if cookies[:stub_user_id]
+      end
+    end
 end
--- spec/main_page_spec.rb
+++ spec/main_page_spec.rb
@@ -1,8 +1,16 @@
 require 'spec_helper'
-include Warden::Test::Helpers
 
 describe "MainPage" do
     before(:each) {
+        @current_user = FactoryGirl.create(:account)
+        # Login as fake user during test
+        # Credit: http://library.edgecase.com/oath-and-capybara 
+        if Capybara.current_driver == :webkit
+          page.driver.browser.set_cookie("stub_user_id=#{@current_user.id}; path=/; domain=127.0.0.1")
+        else
+          cookie_jar = Capybara.current_session.driver.browser.current_session.instance_variable_get(:@rack_mock_session).cookie_jar
+          cookie_jar[:stub_user_id] = @current_user.id
+        end
         visit root_path
     }
     it 'should direct to jobs page when click jobs' do
