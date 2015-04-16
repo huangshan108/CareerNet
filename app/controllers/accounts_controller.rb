4afeddf5d64d53ef98c48b723fab4280cf0b8dec
huangshan108 <shanhuang108@gmail.com>
2015-04-16 13:18:34 -0700

    UX improved

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,6 +3,9 @@ class AccountsController < ApplicationController
     before_action :setup_new_account
 
     def index
+        if logged_in?
+            redirect_to dashboard_path
+        end
     end
 
     def login
