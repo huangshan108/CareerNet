dc2fd5e2b80d8fa50e7a3756f5f827662b11b650
huangshan108 <shanhuang108@gmail.com>
2015-04-26 06:35:26 -0700

    seeds and migrations updated

--- app/models/student.rb
+++ app/models/student.rb
@@ -30,42 +30,44 @@
 require 'will_paginate'
 
 class Student < ActiveRecord::Base
-	belongs_to :college
-	belongs_to :major
-	belongs_to :account
-	belongs_to :company
+  belongs_to :college
+  belongs_to :major
+  belongs_to :account
+  belongs_to :company
 
-	has_many :appointments
-	has_many :staffs, :through => :appointments
+  belongs_to :industry
 
-	has_many :applications
-	has_many :jobs, :through => :applications
-	
-	has_many :interviews
-	has_many :companies, :through => :interviews
-	  
-	has_many :studentskills
-	has_many :skills, through: :studentskills
+  has_many :appointments
+  has_many :staffs, :through => :appointments
 
-    has_many :experiences
+  has_many :applications
+  has_many :jobs, :through => :applications
+  
+  has_many :interviews
+  has_many :companies, :through => :interviews
+    
+  has_many :studentskills
+  has_many :skills, through: :studentskills
+
+  has_many :experiences
 
 
-	def getInterviews(start_date, end_date)
-		self.interviews.select{|i| 
-			i.day >= start_date.to_date and i.day <= end_date.to_date
-		}
-	end
+  def getInterviews(start_date, end_date)
+    self.interviews.select{|i| 
+      i.day >= start_date.to_date and i.day <= end_date.to_date
+    }
+  end
 
-	def getEvents(start_date, end_date)
-		self.account.events.select{ |e|
-			e.time_start >= start_date.to_date and e.time_end <= end_date.to_date
-		}
-	end
+  def getEvents(start_date, end_date)
+    self.account.events.select{ |e|
+      e.time_start >= start_date.to_date and e.time_end <= end_date.to_date
+    }
+  end
 
-	def getAppointments(start_date, end_date)
-		self.appointments.select{|a| 
-			a.day >= start_date.to_date and a.day <= end_date.to_date
-		}
-	end
+  def getAppointments(start_date, end_date)
+    self.appointments.select{|a| 
+      a.day >= start_date.to_date and a.day <= end_date.to_date
+    }
+  end
 
 end
