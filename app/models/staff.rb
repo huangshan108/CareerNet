6b54757d3962ede5fb76ff1f1f80c20b49a38309
Randy Wei <rweics@gmail.com>
2015-05-07 19:19:41 -0700

    Fixed student/staff/company duplication

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -16,20 +16,4 @@ class Staff < ActiveRecord::Base
     has_many :students, :through => :appointments
     belongs_to :account
 
-    def getInterviews(start_date, end_date)
-        []
-    end
-
-    def getEvents(start_date, end_date)
-        self.account.events.select{ |e|
-            e.time_start >= start_date.to_date and e.time_end <= end_date.to_date
-        }
-    end
-
-    def getAppointments(start_date, end_date)
-        self.appointments.select{|a| 
-            a.day >= start_date.to_date and a.day <= end_date.to_date
-        }
-    end
-
 end
